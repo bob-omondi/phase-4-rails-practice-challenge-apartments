c82b8aed9491103d4acd7375e1389b24f23f868a
+ <+>
2022-11-26 18:39:03 +0300

    first updates

---
+++ app/controllers/leases_controller.rb
@@ -0,0 +1,34 @@
+class LeasesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        lease = Lease.create!(lease_params)
+        render json: lease, status: :created
+    end
+
+    def destroy
+        lease = find_lease
+        lease.destroy
+        head :no_content
+    end
+
+    private
+    
+      def lease_params
+        params.permit(:rent)
+      end
+
+      def find_lease
+        Lease.find(params[:id])
+      end
+
+      def render_not_found_response
+        render json: { error: "Lease not found" }, status: :not_found
+      end
+
+      def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+      end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :leases, only: [:create, :destroy]
+
+  resources :tenants, only: [:index, :create, :update, :destroy, :show]
+  
+  resources :apartments, only: [:index, :create, :update, :destroy, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
